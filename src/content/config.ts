5c578b1a1f672996c3dc4c5637f1176a496898a9
Viet Huynh <xa.lach@yahoo.com>
2024-02-22 18:04:03 +0700

    add product price

--- src/content/config.ts
+++ src/content/config.ts
@@ -7,6 +7,7 @@ const products = defineCollection({
       title: z.string().min(1),
       image: image(),
       url: z.string().url(),
+      price: z.number().nonnegative(),
       featured: z.number().min(1).optional(),
     }),
 });
