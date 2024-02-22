360a76dadb83bef6cee321ee30bd6cda83b2b148
Viet Huynh <xa.lach@yahoo.com>
2024-02-22 16:53:34 +0700

    add Vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,16 @@ import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
 import { defineConfig } from "astro/config";
 
+import vercel from "@astrojs/vercel/serverless";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://astro-moon-landing.netlify.app/",
   integrations: [tailwind(), icon()],
   i18n: {
     defaultLocale: "en",
-    locales: ["en", "vi"],
+    locales: ["en", "vi"]
   },
   output: "server",
+  adapter: vercel()
 });
